5dfd5f1e67df7562398ca90d54337d3a8f191472
Kiwicase <emailcaseydent@gmail.com>
2022-07-03 15:32:54 +0300

    Tile Cost Calculator - In progress

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Casey's Supremely Perfect Tile Cost Calculator");
+                Console.WriteLine("");
+                Console.WriteLine("What is the width of the room? (Feet):");
+                String Width = Console.ReadLine();
+                Console.WriteLine("What is the length of the room? (Feet):");
+            }
+
+
+
+        }
+    }
+}
