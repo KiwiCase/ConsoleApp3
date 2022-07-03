8659b9b8eefc4932d739cfa404ef04ac5dc73bd3
Kiwicase <emailcaseydent@gmail.com>
2022-07-03 15:36:50 +0300

    Tile Cost Calculator - In progress

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,9 +8,14 @@
             {
                 Console.WriteLine("Casey's Supremely Perfect Tile Cost Calculator");
                 Console.WriteLine("");
-                Console.WriteLine("What is the width of the room? (Feet):");
+                Console.WriteLine("What is the width of the room? (Feet): ");
                 String Width = Console.ReadLine();
-                Console.WriteLine("What is the length of the room? (Feet):");
+                Console.WriteLine("What is the length of the room? (Feet): ");
+                String Length = Console.ReadLine();
+                Console.WriteLine("What is the size of the tile? (Feet squared): ");
+                String Size = Console.ReadLine();
+                Console.WriteLine("what is the cost per tile? (€): ")
+                String Cost = Console.ReadLine();
             }
 
 
