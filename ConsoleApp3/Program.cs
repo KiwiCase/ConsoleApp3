ace8a9d2a323476d51c87acddb6ae984b15d19f8
Kiwicase <emailcaseydent@gmail.com>
2022-07-04 19:59:10 +0300

    Revert "Tile Cost Calculator - In progress"
    This reverts commit 4d810d5d1c295b334c9476d99cbac5a84e79ed19.

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -2,8 +2,6 @@
 {
     internal class Program
     {
-        private static int v2;
-
         static void Main(string[] args)
         {
             while (true)
@@ -11,20 +9,25 @@
                 Console.WriteLine("Casey's Supremely Perfect Tile Cost Calculator");
                 Console.WriteLine("");
                 Console.WriteLine("What is the width of the room? (Feet): ");
-                int width = Convert.ToInt32(Console.ReadLine());
+                string width = Console.ReadLine();
                 Console.WriteLine("");
                 Console.WriteLine("What is the length of the room? (Feet): ");
-                int length = Convert.ToInt32(Console.ReadLine());
+                string length = Console.ReadLine();
                 Console.WriteLine("");
                 Console.WriteLine("What is the size of the tile? (Feet squared): ");
-                int size = Convert.ToInt32(Console.ReadLine());
+                string size = Console.ReadLine();
                 Console.WriteLine("");
                 Console.WriteLine("What is the cost per tile? ($): ");
-                int cost = Convert.ToInt32(Console.ReadLine());
+                string cost = Console.ReadLine();
 
-                float Area = (width * length);
-                float tNeeded = (Area / size);
-                float TotalCost = cost * tNeeded;
+                float rWidth = (float.Parse(width));
+                float rLength = (float.Parse(length));
+                float tSize = (float.Parse(size));
+                float tCost = (float.Parse(cost));
+
+                float Area = (rWidth * rLength);
+                float tNeeded = (Area / tSize);
+                float TotalCost = (tCost * tNeeded);
 
                 Console.WriteLine("");
                 Console.WriteLine("Area of Tiling = " + Area + " square feet.");
@@ -34,6 +37,9 @@
                 Console.ReadLine();
                 break;
             }
+
+
+
         }
     }
 }
