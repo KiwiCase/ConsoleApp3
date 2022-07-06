8e819709683667186ab052dc8c8a3fb6ad783926
Kiwicase <emailcaseydent@gmail.com>
2022-07-06 17:57:37 +0300

    Tile Cost Calculator - changed int to decimal

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -5,12 +5,11 @@
         static void Main(string[] args)
         {
 
-                Console.WriteLine("Casey's Supremely Perfect Tile Cost Calculator");
-                Console.WriteLine("");
+                Console.WriteLine("Casey's Supremely Perfect Tile Cost Calculator\n");
                 Console.WriteLine("What is the width of the room? (Feet): ");
                 var rWidth = Console.ReadLine();
-                int width;
-                while(!int.TryParse(rWidth, out width))
+                decimal width;
+                while(!decimal.TryParse(rWidth, out width))
                 {
                     Console.WriteLine("Please enter a number or numbers");
                     Console.WriteLine("What is the width of the room? (Feet): ");
@@ -20,8 +19,8 @@
 
                 Console.WriteLine("What is the length of the room? (Feet): ");
                 var rLength = Console.ReadLine();
-                int length;
-                while(!int.TryParse(rLength, out length))
+                decimal length;
+                while(!decimal.TryParse(rLength, out length))
                 {
                     Console.WriteLine("Please enter a number or numbers");
                     Console.WriteLine("What is the length of the room? (Feet): ");
@@ -31,8 +30,8 @@
 
                 Console.WriteLine("What is the size of the tile? (Feet squared): ");
                 var tSize = Console.ReadLine();
-                int size;
-                while(!int.TryParse(tSize, out size))
+                decimal size;
+                while(!decimal.TryParse(tSize, out size))
                 {
                     Console.WriteLine("Please enter a number or numbers");
                     Console.WriteLine("What is the size of the tile? (Feet squared): ");
@@ -42,8 +41,8 @@
 
                 Console.WriteLine("What is the cost per tile? ($): ");
                 var tCost = Console.ReadLine();
-                int cost;
-                while(!int.TryParse(tCost, out cost))
+                decimal cost;
+                while(!decimal.TryParse(tCost, out cost))
                 {
                     Console.WriteLine("Please enter a number or numbers");
                     Console.WriteLine("What is the size of the tile? (Feet squared): ");
@@ -51,9 +50,9 @@
                 }
                 Console.WriteLine("");
 
-                float area = (width * length);
-                float tNeeded = (area / size);
-                float totalCost = cost * tNeeded;
+                decimal area = (width * length);
+                decimal tNeeded = (area / size);
+                decimal totalCost = cost * tNeeded;
 
                 Console.WriteLine("");
                 Console.WriteLine("Area of Tiling = " + area + " square feet.");
