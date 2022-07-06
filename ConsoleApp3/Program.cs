5a54176b58eab6e0289dfffe104ac4aae3cf6fb0
Kiwicase <emailcaseydent@gmail.com>
2022-07-06 09:05:21 +0300

    Tile Cost Calculator - In progress

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -9,26 +9,50 @@
                 Console.WriteLine("Casey's Supremely Perfect Tile Cost Calculator");
                 Console.WriteLine("");
                 Console.WriteLine("What is the width of the room? (Feet): ");
-                int width = Convert.ToInt32(Console.ReadLine());
+                var rWidth = Console.ReadLine();
+                int width;
+                while(!int.TryParse(rWidth, out width))
+                {
+                    Console.WriteLine("Please enter a number or numbers");
+                    rWidth = Console.ReadLine();
+                }              
                 Console.WriteLine("");
+
                 Console.WriteLine("What is the length of the room? (Feet): ");
-                int length = Convert.ToInt32(Console.ReadLine());
+                var rLength = Console.ReadLine();
+                int length;
+                while(!int.TryParse(rLength, out length))
+                {
+                    Console.WriteLine("Please enter a number or numbers");
+                }
                 Console.WriteLine("");
+
                 Console.WriteLine("What is the size of the tile? (Feet squared): ");
-                int size = Convert.ToInt32(Console.ReadLine());
+                var tSize = Console.ReadLine();
+                int size;
+                while(!int.TryParse(tSize, out size))
+                {
+                    Console.WriteLine("Please enter a number or numbers");
+                }
                 Console.WriteLine("");
                 Console.WriteLine("What is the cost per tile? ($): ");
-                int cost = Convert.ToInt32(Console.ReadLine());
+                var tCost = Console.ReadLine();
+                int cost;
+                while(!int.TryParse(tCost, out cost))
+                {
+                    Console.WriteLine("Please enter a number or numbers");
+                }
+                Console.WriteLine("");
 
-                float Area = (width * length);
-                float tNeeded = (Area / size);
-                float TotalCost = cost * tNeeded;
+                float area = (width * length);
+                float tNeeded = (area / size);
+                float totalCost = cost * tNeeded;
 
                 Console.WriteLine("");
-                Console.WriteLine("Area of Tiling = " + Area + " square feet.");
+                Console.WriteLine("Area of Tiling = " + area + " square feet.");
                 Console.WriteLine("Number of Tiles needed = " + tNeeded);
                 Console.WriteLine("");
-                Console.WriteLine("Total Cost = $" + TotalCost);
+                Console.WriteLine("Total Cost = $" + totalCost);
                 Console.ReadLine();
                 break;
             }
